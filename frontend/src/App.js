88af9569312a422a1be50ce659ac4fdb0246bf02
AbiyuTolosa <abiyunigussie7@gmail.com>
2023-04-25 23:03:52 +0300

    routing and navigation is added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,42 @@
+import { Route, Routes } from "react-router-dom";
 import logo from "./logo.svg";
 import Layout from "./components/Layout/Layout";
-
+import LoginPage from "./pages/LoginPage";
+import CreateInvoicePage from "./pages/CreateInvoicePage";
+import CreateProductPage from "./pages/CreateProductPage";
 function App() {
   return (
     <div className="App">
-      <Layout>
+      {/* <Layout>
         <div className="ml-48">Hello</div>
-      </Layout>
+      </Layout> */}
+      <Routes>
+        <Route path="/" element={<Layout></Layout>} />
+        <Route
+          path="/login"
+          element={
+            <Layout>
+              <LoginPage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/create-invoice"
+          element={
+            <Layout>
+              <CreateInvoicePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/create-product"
+          element={
+            <Layout>
+              <CreateProductPage />
+            </Layout>
+          }
+        />
+      </Routes>
     </div>
   );
 }
