bc58d7f3f539b6584263ef006097414c35150c65
AbiyuTolosa <abiyunigussie7@gmail.com>
2023-04-25 23:23:30 +0300

    Create Invoice and Product pages are commited.

---
+++ frontend/src/pages/CreateInvoicePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CreateInvoicePage = () => {
+  return (
+    <div className="container h-full overflow-y-auto">CreateInvoicePage</div>
+  );
+};
+
+export default CreateInvoicePage;
---
+++ frontend/src/pages/CreateProductPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateProductPage = () => {
+  return (
+    <div className="container bg-slate-200 px-10 pt-24 overflow-y-auto">
+      CreateProductPage
+    </div>
+  );
+};
+
+export default CreateProductPage;
