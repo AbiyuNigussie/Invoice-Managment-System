bab4e4e35d1268ec3e06b606910fd0cf4f5f6b1f
AbiyuTolosa <abiyunigussie7@gmail.com>
2023-04-25 23:15:15 +0300

    sub-navigations Linked to different routes.

--- frontend/src/components/Layout/SubNav.js
+++ frontend/src/components/Layout/SubNav.js
@@ -1,14 +1,22 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const CreateSubNav = () => {
   return (
     <div className="flex flex-col bg-slate-950 text-gray-300">
-      <a href="" className="pl-16 py-2  hover:bg-slate-800 hover:text-white">
-        <p>Invoice</p>
-      </a>
-      <a href="" className="pl-16 py-2  hover:bg-slate-800 hover:text-white">
-        <p>Product</p>
-      </a>
+      {/* <a href="" className="pl-16 py-2  hover:bg-slate-800 hover:text-white"> </a> */}
+      <Link
+        to="/create-invoice"
+        className="pl-16 py-2  hover:bg-slate-800 hover:text-white"
+      >
+        Invoice
+      </Link>
+
+      <Link
+        to="/create-product"
+        className="pl-16 py-2  hover:bg-slate-800 hover:text-white"
+      >
+        Product
+      </Link>
     </div>
   );
 };
