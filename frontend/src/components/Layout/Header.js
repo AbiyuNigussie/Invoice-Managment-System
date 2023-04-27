60f5c3bb073952b3bfaa73aba296c42576e8cc1c
AbiyuTolosa <abiyunigussie7@gmail.com>
2023-04-27 13:37:48 +0300

    Header component position changed to sticky

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -7,7 +7,7 @@ import SideBar from "./SideBar";
 const Header = () => {
   const [active, setActive] = useState(false);
   return (
-    <div className="container flex flex-col gap-2 mx-auto absolute">
+    <div className="container flex flex-col gap-2 mx-auto sticky ">
       <div className="flex gap-x-10 px-5 py-5 font-sans bg-slate-800 text-slate-200 text-4xl">
         <BsList className="h-8 w-8 md:hidden" />
         <div className="flex gap-x-1">
