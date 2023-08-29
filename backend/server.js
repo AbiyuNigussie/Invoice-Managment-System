529a5423821e74d2a88be9f574ce1bbc9dbbd54c
AbiyuTolosa <abiyunigussie7@gmail.com>
2023-08-29 15:43:24 +0300

    db connection created, middleware added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
+// Modules
 const express = require("express");
 
+require("dotenv/config");
+
+// App
 const app = express();
 
+// MiddleWare
+app.use(express.json());
+app.use(cors());
+// Import Routes
+const productRoute = require("./routes/products");
+
+// Main Route
 app.get("/", (req, res) => {
   res.send("root path");
 });
 
+// Routes MiddleWare
+app.use("/api/inventory/products", productRoute);
+
+// Database Connection
+mongoose.connect(process.env.DB_CONNECTION);
+
+// Server
 const PORT = 3030;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
